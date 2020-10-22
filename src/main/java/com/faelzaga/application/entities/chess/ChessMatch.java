b1317bef5ca112e88888839ded109bb6225bfa34
Rafael Brito <rafaelgonzagabrito@gmail.com>
2020-10-22 16:00:00 -0300

    Color implement in board printing

--- src/main/java/com/faelzaga/application/entities/chess/ChessMatch.java
+++ src/main/java/com/faelzaga/application/entities/chess/ChessMatch.java
@@ -28,8 +28,18 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('b',6,new Rook(brd, Color.WHITE));
-        placeNewPiece('e',8,new King(brd, Color.BLACK));
-        placeNewPiece('e',1,new King(brd, Color.WHITE));
+        placeNewPiece('c',1, new Rook(brd, Color.WHITE));
+        placeNewPiece('c',2, new Rook(brd, Color.WHITE));
+        placeNewPiece('d',2, new Rook(brd, Color.WHITE));
+        placeNewPiece('e',2, new Rook(brd, Color.WHITE));
+        placeNewPiece('e',1, new Rook(brd, Color.WHITE));
+        placeNewPiece('d',1, new King(brd, Color.WHITE));
+
+        placeNewPiece('c',7, new Rook(brd, Color.BLACK));
+        placeNewPiece('c',8, new Rook(brd, Color.BLACK));
+        placeNewPiece('d',7, new Rook(brd, Color.BLACK));
+        placeNewPiece('e',7, new Rook(brd, Color.BLACK));
+        placeNewPiece('e',8, new Rook(brd, Color.BLACK));
+        placeNewPiece('d',8, new King(brd, Color.BLACK));
     }
 }
